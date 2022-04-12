b60d4bb82561fb76511fee01e045a5e9d9edaa3b
saeree <saeree_club@hotmail.co.th>
2022-04-12 15:56:55 +0700

    add lib vutify

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,34 +1,44 @@
 <template>
-  <div class="hello">
-    <h1 class="text-3xl font-bold underline">
-      {`Hello world!`}
-    </h1>
-  </div>
+  <v-card class="mx-auto" max-width="448">
+    <v-layout>
+      <v-app-bar
+        color="primary"
+        density="compact"
+      >
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Photos</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <v-container fluid>
+          <v-row dense>
+            <v-col
+              v-for="n in 8"
+              :key="n"
+              cols="3"
+            >
+              <v-sheet
+                color="grey-lighten-2"
+                height="96"
+              ></v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+export default {
+  name: 'HelloWorld',
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./index.css";
-import vuetify from '@/plugins/vuetify' // path to vuetify export
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount("#app");
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
